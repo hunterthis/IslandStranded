a7217f56d16d2bf2448b338d8610aebdd2169b4e
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-08-03 22:29:13 -0700

    Created User Class.

---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslandStranded
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        //[Index(IsUnique = true)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required] // need to make default value 0
+        public int CurrentStoryEventValue { get; set; }
+
+    }
+}
