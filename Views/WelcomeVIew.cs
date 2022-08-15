5f043b2cab6c8dc57def2b47a2307ebd2a855f6d
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-08-15 07:01:41 -0700

    Created the WelcomeVIew using the Spectre.Console package. Calls the IView interface. Created The users choices.  Defined the execute(). It creates a prompt, usin Spectre.Console, to add a .Title and .AddChoices. added switch to exit and give error.

---
+++ Views/WelcomeVIew.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IslandStranded.Views;
+
+public class WelcomeVIew : IView
+{
+    private readonly GlobalState _state;
+
+    public WelcomeVIew(GlobalState state)
+    {
+        _state = state;
+    }
+
+    enum WelcomeViewChoice
+    {
+        LogIn, // View
+        CreateAccount, // View
+        Quit
+    }
+
+    
+    public void Execute()
+    {
+        var choice = AnsiConsole.Prompt
+            (
+                new SelectionPrompt<WelcomeViewChoice>()
+                    .Title("Welcome to our game. What will you do next?") // Tile/ intro text
+                    .AddChoices(Enum.GetValues<WelcomeViewChoice>()) // choices from the WelcomViewChoice
+            );
+
+        switch (choice)
+        {
+            case WelcomeViewChoice.Quit:
+                _state.ShouldExit = true;
+                return;
+
+            default:
+                Console.WriteLine("Error: hit defaukt case on LogInView");
+                return;
+        }
+        
+    }
+}
