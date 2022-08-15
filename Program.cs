a53a2161c0b708f988db7f1b8818a2ab8cbb50a8
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-08-15 06:51:59 -0700

    Calls the global state and the Welcome VIew.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using IslandStranded.Views; // use views folder
+
+namespace IslandStranded;
+
+public static class Program
+{
+    public static void Main()
+    {
+        //var db = new database()
+        var state = new GlobalState();
+        //state.Database = db;
+        state.CurrentView = new WelcomeVIew(state);
+
+        while (!state.ShouldExit)
+        {
+            state.CurrentView.Execute();
+        }
+    }
+}
